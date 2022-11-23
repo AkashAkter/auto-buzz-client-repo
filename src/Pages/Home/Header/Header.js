c58ac71c455d653025de388f8bf6c4b154d6004e
Akash Akter <AkashAkter>
2022-11-23 23:50:35 +0600

    banner added

--- src/Pages/Home/Header/Header.js
+++ src/Pages/Home/Header/Header.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import './Header.css'
 
 const Header = () => {
     return (
         <div>
+            <div className="home-banner  flex items-center justify-center">
+                <h1 className="banner-title text-center text-white text-7xl">
+                    <span className='text-[#ff4605]'>AKASH</span>AKTER
+                </h1>
 
+            </div>
         </div>
     );
 };
